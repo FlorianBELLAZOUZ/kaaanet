e663322c6e699818fd3f12a81deb2bf5f4d2bffd
Florian Bellazouz <bellazouz.florian@gmail.com>
2016-01-21 19:04:37 +0100

    convert tab to space

--- craftApi.js
+++ craftApi.js
@@ -2,33 +2,33 @@
 var packet = require('kaaanet').packet;
 
 var alpha = packet.create([
-	{
-		name:'close'
-	},
-	{
-		name:'profile',
-		id:string(60),
-		pseudo:string(20),
-		lvl:integral(10),
-		type:string(3)
-	},
-	{
-		name:'input',
-		type:string(3),
-		space:integral(1),
-		key:arrayString(3),
-		mana:arrayObject({
-			type:string(4),
-			nice:string(3),
-		})
-	}
+  {
+    name:'close'
+  },
+  {
+    name:'profile',
+    id:string(60),
+    pseudo:string(20),
+    lvl:integral(10),
+    type:string(3)
+  },
+  {
+    name:'input',
+    type:string(3),
+    space:integral(1),
+    key:arrayString(3),
+    mana:arrayObject({
+      type:string(4),
+      nice:string(3),
+    })
+  }
 ]);
 
 packetBeta.init([
-	{
-		name:'createAComplexeValue',
-		nice:string(3)
-	}
+  {
+    name:'createAComplexeValue',
+    nice:string(3)
+  }
 ], alpha); // heritage de packet
 
 packetBeta.extend(alpha); // override same name
@@ -44,11 +44,11 @@ var server = new Server({port:8080,packets:publicPackets})
 server.initPackets(publicPackets);
 
 server.on('connection', function(client) {
-	client.send.input({type:'feu', space:true, key:['bla','foo','bee']});
-	client.send.close();
+  client.send.input({type:'feu', space:true, key:['bla','foo','bee']});
+  client.send.close();
 
-	client.on('input', input)
-	client.get.input({type:'feu'}, inputGet);
+  client.on('input', input)
+  client.get.input({type:'feu'}, inputGet);
 });
 
 server.on('close', function(data client) {});
@@ -57,20 +57,20 @@ server.send.close();
 server.send.input({type:32,space:true});
 
 function input(data, client) {
-	if(data.type == 'feu'){ }
+  if(data.type == 'feu'){ }
 
-	client.send.close();
+  client.send.close();
 }
 
 function inputGet(data, client) {
-	if(data.type == 'ok')
-	this.clientsFeu.push(client);
+  if(data.type == 'ok')
+  this.clientsFeu.push(client);
 }
 
 
 // function sendTo(){
-// 	server.room('nice').send.close();
-// 	server.room('nice').state = 'ready';
+//  server.room('nice').send.close();
+//  server.room('nice').state = 'ready';
 // }
 
 // client.goRoom('nice')
@@ -89,15 +89,15 @@ client.send.close();
 client.send.input({x:10,y:40,z:40});
 
 client.on('input', function(data){
-	this.send.input({x:10,y:40,z:34});
+  this.send.input({x:10,y:40,z:34});
 });
 
 client.get.profile(function(data){
-	save.data = data;
+  save.data = data;
 });
 
 client.get.profile({id:30}, function(data){
-	save.data = data;
+  save.data = data;
 });
 
 
