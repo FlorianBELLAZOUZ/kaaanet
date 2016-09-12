348cef56fa4830f3070fab7d42b4cc885999067e
florian bellazouz <florian.bellazouz@gmail.com>
2016-09-12 08:48:56 +0200

    add index - .addPackets(ws, packets)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Packet = exports.Packet = require('./lib/Packet');
 exports.Client = require('./lib/Client');
 exports.Server = require('./lib/Server');
+exports.addPackets = require('./lib/Server')._addPacket;
 exports.ArrayInt = Packet.ArrayInt;
 exports.ArrayTxt = Packet.ArrayTxt;
 exports.Int = Packet.Int;
